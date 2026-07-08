5fae7c4287a96f1cdeb0a876b87300794f28fedd
Ruchit <296442514+Ruchit-Developer@users.noreply.github.com>
2026-07-08 12:19:18 +0530

    Delete src directory

--- src/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const config = {
-    slackWebhookUrl: process.env.SLACK_WEBHOOK_URL || '',
-    githubSecret: process.env.GITHUB_SECRET || ''
-};
-
-module.exports = config;
